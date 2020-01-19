6ee1587e91c86121317da5700556461bc05596a6
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-19 13:23:00 +0000

    add home module and landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-reigans';
+export class AppComponent implements OnInit {
+  isLandingPage: boolean;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        tap(event => {
+          if (event instanceof NavigationStart) {
+            // Don't show header and nav-bar
+            this.isLandingPage = event.url === '/';
+          }
+        }),
+      )
+      .subscribe();
+  }
 }
