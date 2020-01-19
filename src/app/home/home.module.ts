f47cf62e90f6123a2271035bb318e9c8a3e91dc1
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-19 21:39:00 +0000

    add ad section to home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NguCarouselModule } from '@ngu/carousel';
+import {ClarityModule} from '@clr/angular';
 
 import { HomeRoutingModule } from './home-routing.module';
 
@@ -10,6 +11,6 @@ import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [LandingComponent, HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, NguCarouselModule],
+  imports: [CommonModule, HomeRoutingModule, NguCarouselModule, ClarityModule],
 })
 export class HomeModule {}
