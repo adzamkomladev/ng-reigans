2366a0f9958b96aeb021732f9a9a428447228ee4
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-20 00:38:41 +0000

    add display products and fix bootstrap dep not found error

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,9 +8,11 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { LandingComponent } from './pages/landing/landing.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DisplayProductsComponent } from './components/display-products/display-products.component';
+import { DisplayProductComponent } from './components/display-product/display-product.component';
 
 @NgModule({
-  declarations: [LandingComponent, HomeComponent],
+  declarations: [LandingComponent, HomeComponent, DisplayProductsComponent, DisplayProductComponent],
   imports: [CommonModule, HomeRoutingModule, NguCarouselModule, ClarityModule],
 })
 export class HomeModule {}
