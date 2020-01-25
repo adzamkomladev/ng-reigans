b9e4611d66a726966d154b9319aca2ec6d823ca8
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-25 04:30:02 +0000

    add load more feature

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -8,7 +8,8 @@ import {
 
 import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
 
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { FilterByCategoryService } from '../../services/filter-by-category.service';
 
@@ -23,12 +24,50 @@ export class HomeComponent implements OnInit, AfterViewInit {
   carouselConfig: NguCarouselConfig;
   carouselItems: Observable<{ id: number; url: string }[]>;
   categories: Observable<string[]>;
+  topSellingProductsSubject: BehaviorSubject<
+    { name: string; price: number; category: string }[]
+  >;
   latestProducts: Observable<
     { name: string; price: number; category: string }[]
   >;
   topSellingProducts: Observable<
     { name: string; price: number; category: string }[]
   >;
+
+  allProducts = [
+    { name: 'Dakashi', price: 200, category: 'Dress' },
+    { name: 'Herbi', price: 213, category: 'Co-ord Set' },
+    { name: 'Don pre', price: 500, category: 'Three Piece Set' },
+    { name: 'Hijabi', price: 400, category: 'Gown' },
+    { name: 'Harvey', price: 222, category: 'Gown' },
+    { name: 'A Barbi', price: 324, category: 'Layers' },
+    { name: 'Rir in arari', price: 100.29, category: 'Dashiki' },
+    { name: 'Finessers', price: 89.42, category: 'Scarf' },
+    { name: 'Body amour', price: 323.99, category: 'Layers' },
+    { name: 'Improvidse', price: 250.99, category: 'Dress' },
+    { name: 'Sleeep at night', price: 504, category: 'Dashiki' },
+    { name: 'Fight for your right', price: 208, category: 'Three Piece Set' },
+    { name: 'Helli', price: 120, category: 'Two Piece Set' },
+    { name: 'Goal', price: 123, category: 'Two Piece Set' },
+    { name: 'Drink', price: 299, category: 'Co-ord Set' },
+    { name: 'Hoodie', price: 450, category: 'Dress' },
+    { name: 'Dakashi', price: 200, category: 'Dress' },
+    { name: 'Herbi', price: 213, category: 'Co-ord Set' },
+    { name: 'Don pre', price: 500, category: 'Three Piece Set' },
+    { name: 'Hijabi', price: 400, category: 'Gown' },
+    { name: 'Harvey', price: 222, category: 'Gown' },
+    { name: 'A Barbi', price: 324, category: 'Layers' },
+    { name: 'Rir in arari', price: 100.29, category: 'Dashiki' },
+    { name: 'Finessers', price: 89.42, category: 'Scarf' },
+    { name: 'Body amour', price: 323.99, category: 'Layers' },
+    { name: 'Improvidse', price: 250.99, category: 'Dress' },
+    { name: 'Sleeep at night', price: 504, category: 'Dashiki' },
+    { name: 'Fight for your right', price: 208, category: 'Three Piece Set' },
+    { name: 'Helli', price: 120, category: 'Two Piece Set' },
+    { name: 'Goal', price: 123, category: 'Two Piece Set' },
+    { name: 'Drink', price: 299, category: 'Co-ord Set' },
+    { name: 'Hoodie', price: 450, category: 'Dress' },
+  ];
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -43,9 +82,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
       velocity: 0.2,
       animation: 'lazy',
     };
+
+    this.topSellingProductsSubject = new BehaviorSubject<
+      { name: string; price: number; category: string }[]
+    >(this.allProducts.slice(0, 8));
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.carouselItems = of([
       { id: 1, url: '/assets/img/c1.jpg' },
       { id: 2, url: '/assets/img/c2.jpg' },
@@ -66,32 +109,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
       'Scarf',
       'Layers',
     ]);
-    const allProducts = [
-      { name: 'Dakashi', price: 200, category: 'Dress' },
-      { name: 'Herbi', price: 213, category: 'Co-ord Set' },
-      { name: 'Don pre', price: 500, category: 'Three Piece Set' },
-      { name: 'Hijabi', price: 400, category: 'Gown' },
-      { name: 'Harvey', price: 222, category: 'Gown' },
-      { name: 'A Barbi', price: 324, category: 'Layers' },
-      { name: 'Rir in arari', price: 100.29, category: 'Dashiki' },
-      { name: 'Finessers', price: 89.42, category: 'Scarf' },
-      { name: 'Body amour', price: 323.99, category: 'Layers' },
-      { name: 'Improvidse', price: 250.99, category: 'Dress' },
-      { name: 'Sleeep at night', price: 504, category: 'Dashiki' },
-      { name: 'Fight for your right', price: 208, category: 'Three Piece Set' },
-      { name: 'Helli', price: 120, category: 'Two Piece Set' },
-      { name: 'Goal', price: 123, category: 'Two Piece Set' },
-      { name: 'Drink', price: 299, category: 'Co-ord Set' },
-      { name: 'Hoodie', price: 450, category: 'Dress' },
-    ];
-    this.latestProducts = of(allProducts);
 
-    this.topSellingProducts = this.filterByCategoryService.filterProducts(
-      allProducts,
-    );
+    this.latestProducts = of(this.allProducts.slice(0, 8));
+
+    this.topSellingProducts = this.topSellingProductsSubject
+      .asObservable()
+      .pipe(
+        switchMap(topSellingProducts =>
+          this.filterByCategoryService.filterProducts(topSellingProducts),
+        ),
+      );
   }
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
     this.cdr.detectChanges();
   }
 
@@ -101,5 +131,16 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   onFilterByCategory(category: string): void {
     this.filterByCategoryService.setFilterCategory(category);
+  }
+
+  onLoadMore(): void {
+    this.filterByCategoryService.setFilterCategory('ALL');
+
+    this.topSellingProductsSubject.next(
+      this.allProducts.slice(
+        0,
+        this.topSellingProductsSubject.value.length + 8,
+      ),
+    );
   }
 }
