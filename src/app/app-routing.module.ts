d5b287bc126797e947f25efe882ac94098dc5243
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-29 23:57:51 +0000

    add categories filter controls and price filter range control

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
   },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then(m => m.ProductsModule),
+  },
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,12 +6,13 @@ import { ClarityModule } from '@clr/angular';
 
 import { HomeRoutingModule } from './home-routing.module';
 
+import { ScrollToDirective } from './directives/scroll-to.directive';
+import { InViewportDirective } from './directives/in-viewport.directive';
+
 import { LandingComponent } from './pages/landing/landing.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DisplayProductsComponent } from './components/display-products/display-products.component';
 import { DisplayProductComponent } from './components/display-product/display-product.component';
-import { ScrollToDirective } from './directives/scroll-to.directive';
-import { InViewportDirective } from './directives/in-viewport.directive';
 import { FilterButtonsSectionComponent } from './components/filter-buttons-section/filter-buttons-section.component';
 
 @NgModule({
