1f4cee09716752f4464aed3d3481dad96846fbe8
adzamkomladev <adzamkomla.dev@gmail.com>
2020-01-30 21:06:55 +0000

    add size filter to products page

--- src/app/products/pages/products/products.component.ts
+++ src/app/products/pages/products/products.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-price: number;
+  price: number;
+  sizes: number[];
   constructor() {
     this.price = 600;
+
+    this.sizes = [33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
