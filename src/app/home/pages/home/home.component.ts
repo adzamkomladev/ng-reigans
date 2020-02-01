4e5f108cd8696241ed30b5b3676d5e9332f9c49b
adzamkomladev <adzamkomla.dev@gmail.com>
2020-02-01 16:57:42 +0000

    add product service

--- src/app/home/pages/home/home.component.ts
+++ src/app/home/pages/home/home.component.ts
@@ -9,8 +9,9 @@ import {
 import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
 
 import { BehaviorSubject, Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 
+import { ProductService } from '../../../core/services/product.service';
 import { FilterByCategoryService } from '../../../core/services/filter-by-category.service';
 
 import { Product } from '../../../core/interfaces/product';
@@ -26,210 +27,18 @@ export class HomeComponent implements OnInit, AfterViewInit {
   carouselConfig: NguCarouselConfig;
   carouselItems: Observable<{ id: number; url: string }[]>;
   categories: Observable<string[]>;
-  topSellingProductsSubject: BehaviorSubject<Product[]>;
   latestProducts: Observable<Product[]>;
   topSellingProducts: Observable<Product[]>;
+  topSellingProductsSubject: BehaviorSubject<Product[]>;
+  productsSubject: BehaviorSubject<Product[]>;
 
-  allProducts: Product[] = [
-    { id: 1, name: 'Dakashi', price: 200, category: 'Dress', sizes: [43, 37] },
-    {
-      id: 2,
-      name: 'Herbi',
-      price: 213,
-      category: 'Co-ord Set',
-      sizes: [41, 39],
-    },
-    {
-      id: 3,
-      name: 'Don pre',
-      price: 500,
-      category: 'Three Piece Set',
-      sizes: [40, 44],
-    },
-    {
-      id: 4,
-      name: 'Hijabi',
-      price: 400,
-      category: 'Gown',
-      sizes: [43, 36, 37],
-    },
-
-    {
-      id: 5,
-      name: 'A Barbi',
-      price: 324,
-      category: 'Layers',
-      sizes: [43, 44, 45, 46, 38, 37],
-    },
-    {
-      id: 6,
-      name: 'Rir in arari',
-      price: 100.29,
-      category: 'Dashiki',
-      sizes: [40, 38],
-    },
-    { id: 7, name: 'Finessers', price: 89.42, category: 'Scarf', sizes: [42] },
-    {
-      id: 8,
-      name: 'Body amour',
-      price: 323.99,
-      category: 'Layers',
-      sizes: [41, 34],
-    },
-    {
-      id: 9,
-      name: 'Improvidse',
-      price: 250.99,
-      category: 'Dress',
-      sizes: [43, 44, 45, 41, 39, 36, 37],
-    },
-    {
-      id: 10,
-      name: 'Sleeep at night',
-      price: 504,
-      category: 'Dashiki',
-      sizes: [43, 44, 45, 46, 38, 37],
-    },
-    {
-      id: 11,
-      name: 'Fight for your right',
-      price: 208,
-      category: 'Three Piece Set',
-      sizes: [43, 44, 45, 37],
-    },
-    {
-      id: 12,
-      name: 'Helli',
-      price: 120,
-      category: 'Two Piece Set',
-      sizes: [38],
-    },
-    {
-      id: 13,
-      name: 'Goal',
-      price: 123,
-      category: 'Two Piece Set',
-      sizes: [45],
-    },
-    {
-      id: 14,
-      name: 'Drink',
-      price: 299,
-      category: 'Co-ord Set',
-      sizes: [43, 44, 45, 46, 38, 36, 41, 37],
-    },
-    {
-      id: 15,
-      name: 'Hoodie',
-      price: 450,
-      category: 'Dress',
-      sizes: [43, 44, 45, 46, 37],
-    },
-    {
-      id: 16,
-      name: 'Dakashi',
-      price: 200,
-      category: 'Dress',
-      sizes: [43, 41, 40, 40, 38, 37],
-    },
-    {
-      id: 17,
-      name: 'Herbi',
-      price: 213,
-      category: 'Co-ord Set',
-      sizes: [43, 44],
-    },
-    {
-      id: 18,
-      name: 'Don pre',
-      price: 500,
-      category: 'Three Piece Set',
-      sizes: [36, 37],
-    },
-    { id: 19, name: 'Hijabi', price: 400, category: 'Gown', sizes: [37] },
-    { id: 20, name: 'Harvey', price: 222, category: 'Gown', sizes: [36] },
-    {
-      id: 21,
-      name: 'A Barbi',
-      price: 324,
-      category: 'Layers',
-      sizes: [40, 38, 39],
-    },
-    {
-      id: 22,
-      name: 'Rir in arari',
-      price: 100.29,
-      category: 'Dashiki',
-      sizes: [41, 38, 39],
-    },
-    {
-      id: 23,
-      name: 'Finessers',
-      price: 89.42,
-      category: 'Scarf',
-      sizes: [40, 36],
-    },
-    {
-      id: 24,
-      name: 'Body amour',
-      price: 323.99,
-      category: 'Layers',
-      sizes: [47],
-    },
-    {
-      id: 25,
-      name: 'Improvidse',
-      price: 250.99,
-      category: 'Dress',
-      sizes: [41],
-    },
-    {
-      id: 26,
-      name: 'Sleeep at night',
-      price: 504,
-      category: 'Dashiki',
-      sizes: [40, 42],
-    },
-    {
-      id: 27,
-      name: 'Fight for your right',
-      price: 208,
-      category: 'Three Piece Set',
-      sizes: [38, 39],
-    },
-    {
-      id: 28,
-      name: 'Helli',
-      price: 120,
-      category: 'Two Piece Set',
-      sizes: [40, 43, 39],
-    },
-    {
-      id: 29,
-      name: 'Goal',
-      price: 123,
-      category: 'Two Piece Set',
-      sizes: [37, 46],
-    },
-    {
-      id: 30,
-      name: 'Drink',
-      price: 299,
-      category: 'Co-ord Set',
-      sizes: [40, 34, 39],
-    },
-    {
-      id: 31,
-      name: 'Hoodie',
-      price: 450,
-      category: 'Dress',
-      sizes: [40, 35, 39],
-    },
-    { id: 32, name: 'Harvey', price: 222, category: 'Gown', sizes: [39] },
-  ];
+  get products(): Observable<Product[]> {
+    return this.productsSubject.asObservable();
+  }
 
   constructor(
     private cdr: ChangeDetectorRef,
+    private productService: ProductService,
     private filterByCategoryService: FilterByCategoryService,
   ) {
     this.carouselConfig = {
@@ -242,9 +51,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
       animation: 'lazy',
     };
 
-    this.topSellingProductsSubject = new BehaviorSubject<Product[]>(
-      this.allProducts.slice(0, 8),
-    );
+    this.productsSubject = new BehaviorSubject<Product[]>([]);
+    this.topSellingProductsSubject = new BehaviorSubject<Product[]>([]);
   }
 
   ngOnInit(): void {
@@ -269,7 +77,13 @@ export class HomeComponent implements OnInit, AfterViewInit {
       'Layers',
     ]);
 
-    this.latestProducts = of(this.allProducts.slice(0, 8));
+    this.latestProducts = this.productService.getAll().pipe(
+      tap(products => {
+        this.productsSubject.next(products);
+        this.topSellingProductsSubject.next(products.slice(0, 8));
+      }),
+      map(products => products.slice(0, 8)),
+    );
 
     this.topSellingProducts = this.topSellingProductsSubject
       .asObservable()
@@ -296,7 +110,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.filterByCategoryService.setFilterCategory('ALL');
 
     this.topSellingProductsSubject.next(
-      this.allProducts.slice(
+      this.productsSubject.value.slice(
         0,
         this.topSellingProductsSubject.value.length + 8,
       ),
--- src/app/products/pages/products/products.component.ts
+++ src/app/products/pages/products/products.component.ts
@@ -8,6 +8,7 @@ import { FilterBySizeService } from '../../services/filter-by-size.service';
 import { FilterByPriceService } from '../../services/filter-by-price.service';
 
 import { Product } from '../../../core/interfaces/product';
+import { ProductService } from '../../../core/services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -18,210 +19,9 @@ export class ProductsComponent implements OnInit {
   price: number;
   sizes: number[];
   categories: Observable<string[]>;
-  filteredProductsSubject: BehaviorSubject<
-    Product[]
-  >;
-  filteredProducts: Observable<
-    Product[]
-  >;
-
-  allProducts: Product[] = [
-    { id: 1, name: 'Dakashi', price: 200, category: 'Dress', sizes: [43, 37] },
-    {
-      id: 2,
-      name: 'Herbi',
-      price: 213,
-      category: 'Co-ord Set',
-      sizes: [41, 39],
-    },
-    {
-      id: 3,
-      name: 'Don pre',
-      price: 500,
-      category: 'Three Piece Set',
-      sizes: [40, 44],
-    },
-    {
-      id: 4,
-      name: 'Hijabi',
-      price: 400,
-      category: 'Gown',
-      sizes: [43, 36, 37],
-    },
-
-    {
-      id: 5,
-      name: 'A Barbi',
-      price: 324,
-      category: 'Layers',
-      sizes: [43, 44, 45, 46, 38, 37],
-    },
-    {
-      id: 6,
-      name: 'Rir in arari',
-      price: 100.29,
-      category: 'Dashiki',
-      sizes: [40, 38],
-    },
-    { id: 7, name: 'Finessers', price: 89.42, category: 'Scarf', sizes: [42] },
-    {
-      id: 8,
-      name: 'Body amour',
-      price: 323.99,
-      category: 'Layers',
-      sizes: [41, 34],
-    },
-    {
-      id: 9,
-      name: 'Improvidse',
-      price: 250.99,
-      category: 'Dress',
-      sizes: [43, 44, 45, 41, 39, 36, 37],
-    },
-    {
-      id: 10,
-      name: 'Sleeep at night',
-      price: 504,
-      category: 'Dashiki',
-      sizes: [43, 44, 45, 46, 38, 37],
-    },
-    {
-      id: 11,
-      name: 'Fight for your right',
-      price: 208,
-      category: 'Three Piece Set',
-      sizes: [43, 44, 45, 37],
-    },
-    {
-      id: 12,
-      name: 'Helli',
-      price: 120,
-      category: 'Two Piece Set',
-      sizes: [38],
-    },
-    {
-      id: 13,
-      name: 'Goal',
-      price: 123,
-      category: 'Two Piece Set',
-      sizes: [45],
-    },
-    {
-      id: 14,
-      name: 'Drink',
-      price: 299,
-      category: 'Co-ord Set',
-      sizes: [43, 44, 45, 46, 38, 36, 41, 37],
-    },
-    {
-      id: 15,
-      name: 'Hoodie',
-      price: 450,
-      category: 'Dress',
-      sizes: [43, 44, 45, 46, 37],
-    },
-    {
-      id: 16,
-      name: 'Dakashi',
-      price: 200,
-      category: 'Dress',
-      sizes: [43, 41, 40, 40, 38, 37],
-    },
-    {
-      id: 17,
-      name: 'Herbi',
-      price: 213,
-      category: 'Co-ord Set',
-      sizes: [43, 44],
-    },
-    {
-      id: 18,
-      name: 'Don pre',
-      price: 500,
-      category: 'Three Piece Set',
-      sizes: [36, 37],
-    },
-    { id: 19, name: 'Hijabi', price: 400, category: 'Gown', sizes: [37] },
-    { id: 20, name: 'Harvey', price: 222, category: 'Gown', sizes: [36] },
-    {
-      id: 21,
-      name: 'A Barbi',
-      price: 324,
-      category: 'Layers',
-      sizes: [40, 38, 39],
-    },
-    {
-      id: 22,
-      name: 'Rir in arari',
-      price: 100.29,
-      category: 'Dashiki',
-      sizes: [41, 38, 39],
-    },
-    {
-      id: 23,
-      name: 'Finessers',
-      price: 89.42,
-      category: 'Scarf',
-      sizes: [40, 36],
-    },
-    {
-      id: 24,
-      name: 'Body amour',
-      price: 323.99,
-      category: 'Layers',
-      sizes: [47],
-    },
-    {
-      id: 25,
-      name: 'Improvidse',
-      price: 250.99,
-      category: 'Dress',
-      sizes: [41],
-    },
-    {
-      id: 26,
-      name: 'Sleeep at night',
-      price: 504,
-      category: 'Dashiki',
-      sizes: [40, 42],
-    },
-    {
-      id: 27,
-      name: 'Fight for your right',
-      price: 208,
-      category: 'Three Piece Set',
-      sizes: [38, 39],
-    },
-    {
-      id: 28,
-      name: 'Helli',
-      price: 120,
-      category: 'Two Piece Set',
-      sizes: [40, 43, 39],
-    },
-    {
-      id: 29,
-      name: 'Goal',
-      price: 123,
-      category: 'Two Piece Set',
-      sizes: [37, 46],
-    },
-    {
-      id: 30,
-      name: 'Drink',
-      price: 299,
-      category: 'Co-ord Set',
-      sizes: [40, 34, 39],
-    },
-    {
-      id: 31,
-      name: 'Hoodie',
-      price: 450,
-      category: 'Dress',
-      sizes: [40, 35, 39],
-    },
-    { id: 32, name: 'Harvey', price: 222, category: 'Gown', sizes: [39] },
-  ];
+  filteredProducts: Observable<Product[]>;
+  productsSubject: BehaviorSubject<Product[]>;
+  filteredProductsSubject: BehaviorSubject<Product[]>;
 
   get filterCategory(): Observable<string> {
     return this.filterByCategoryService.filterCategory;
@@ -232,6 +32,7 @@ export class ProductsComponent implements OnInit {
   }
 
   constructor(
+    private productService: ProductService,
     private filterByCategoryService: FilterByCategoryService,
     private filterBySizeService: FilterBySizeService,
     private filterByPriceService: FilterByPriceService,
@@ -240,9 +41,8 @@ export class ProductsComponent implements OnInit {
 
     this.sizes = [33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48];
 
-    this.filteredProductsSubject = new BehaviorSubject<
-      Product[]
-    >(this.allProducts.slice(0, 8));
+    this.filteredProductsSubject = new BehaviorSubject<Product[]>([]);
+    this.productsSubject = new BehaviorSubject<Product[]>([]);
   }
 
   ngOnInit() {
@@ -257,6 +57,17 @@ export class ProductsComponent implements OnInit {
       'Scarf',
       'Layers',
     ]);
+
+    this.productService
+      .getAll()
+      .pipe(
+        take(1),
+        tap(products => {
+          this.productsSubject.next(products);
+          this.filteredProductsSubject.next(products.slice(0, 7));
+        }),
+      )
+      .subscribe();
 
     this.filteredProducts = this.filteredProductsSubject.asObservable().pipe(
       switchMap(filteredProducts =>
@@ -314,12 +125,18 @@ export class ProductsComponent implements OnInit {
       sizes: number[];
     }[],
   ): boolean {
-    return products.length < this.allProducts.length && products.length !== 0;
+    return (
+      products.length < this.productsSubject.value.length &&
+      products.length !== 0
+    );
   }
 
   onLoadMore(): void {
     this.filteredProductsSubject.next(
-      this.allProducts.slice(0, this.filteredProductsSubject.value.length + 8),
+      this.productsSubject.value.slice(
+        0,
+        this.filteredProductsSubject.value.length + 6,
+      ),
     );
   }
 }
