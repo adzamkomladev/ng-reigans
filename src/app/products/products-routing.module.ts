e09354909eca1dba7d427fde18062e533484d097
adzamkomladev <adzamkomla.dev@gmail.com>
2020-02-01 13:49:34 +0000

    add product page routing

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: ':id', component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,9 +7,10 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductComponent],
   imports: [CommonModule, ProductsRoutingModule, FormsModule, SharedModule],
 })
 export class ProductsModule {}
