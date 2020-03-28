fe7b9b7aceaf6c4dc8f4837a7a9ea3283c640fdc
adzamkomladev <adzamkomla.dev@gmail.com>
2020-03-28 02:46:44 +0000

    Add check-out form to check-out page

--- src/app/features/check-out/check-out.module.ts
+++ src/app/features/check-out/check-out.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import { SharedModule } from '../../shared/shared.module';
 import { CheckOutRoutingModule } from './check-out-routing.module';
 
 import { CheckOutComponent } from './check-out.component';
+import { CheckOutFormComponent } from './components/check-out-form/check-out-form.component';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 
 @NgModule({
-  declarations: [CheckOutComponent],
-  imports: [CommonModule, CheckOutRoutingModule],
+  declarations: [
+    CheckOutComponent,
+    CheckOutFormComponent,
+    CartSummaryComponent,
+  ],
+  imports: [CommonModule, CheckOutRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
 })
 export class CheckOutModule {}
---
+++ src/app/features/check-out/components/check-out-form/check-out-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-check-out-form',
+  templateUrl: './check-out-form.component.html',
+  styleUrls: ['./check-out-form.component.scss'],
+})
+export class CheckOutFormComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: this.formBuilder.group({
+        first: ['Luke', Validators.required],
+        last: ['Skywalker', Validators.required],
+      }),
+      contact: this.formBuilder.group({
+        email: [],
+        phone: [],
+      }),
+      password: this.formBuilder.group({
+        password: [],
+        confirm: [],
+      }),
+    });
+  }
+
+  ngOnInit() {}
+
+  submit() {
+    console.log('reactive form submit', this.form.value);
+  }
+}
